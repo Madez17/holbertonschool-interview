6a19947caefe629e5768a2a6dd314935644ddf70
Madez17 <mafe.mendez.m@gmail.com>
2020-07-21 15:12:55 -0500

    Algorith Boxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -3,6 +3,7 @@ def canUnlockAll(boxes):
     keys = [0]
     for list in boxes:
         for key in list:
-            if(key != boxes.index(list) and key not in keys):
+            if(key != boxes.index(list) and key not in keys and
+               key < len(boxes)):
                 keys.append(key)
     return len(boxes) == len(keys)
