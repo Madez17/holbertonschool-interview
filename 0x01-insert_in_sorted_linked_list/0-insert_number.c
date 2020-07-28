534be410f83aa30ac5b81a599ff7c8556a482dcb
Madez17 <mafe.mendez.m@gmail.com>
2020-07-28 20:08:54 +0000

    Insert in sorted linked list

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * insert_node - Function insert node
+ *
+ * @head: Pointer to the head node(Initial).
+ * @number:  Data type.
+ *
+ * Return: new_node.
+*/
+
+listint_t *insert_node(listint_t **head, int number)
+{
+
+	listint_t *iter = *head;
+
+	listint_t *new_node = malloc(sizeof(listint_t));
+
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->next = NULL;
+	new_node = number;
+
+	if (*iter == NULL)
+	{
+		(*iter) = new_node;
+		return (*iter);
+	}
+	
+	while (iter->next != NULL)
+	{
+		iter = iter->next;
+		if (iter->next > new_node)
+		{
+			new_node->next = iter;
+			iter = new_node;
+		}
+		else
+		{
+			iter->next = new_node;
+			new_node->next = NULL;
+		}
+		iter->next++;
+		return (new_node);
+	}
+	return (0);
+}
