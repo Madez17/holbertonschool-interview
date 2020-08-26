4fce696da8e61685d991dac1932456f8279e1f82
Madez17 <mafe.mendez.m@gmail.com>
2020-08-26 01:15:58 +0000

    Linked list Palindrome

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - Function checks if a singly linked list is a palindrome
+ *
+ * @head: Pointer to the head node(Initial).
+ *
+ * Return: 0.
+*/
+
+int is_palindrome(listint_t **head)
+{
+
+}
