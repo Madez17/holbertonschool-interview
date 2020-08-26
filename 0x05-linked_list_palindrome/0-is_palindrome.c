5a8c3ad4a617fc6b4b620e623fc3f6ec3433a19f
Madez17 <mafe.mendez.m@gmail.com>
2020-08-26 03:27:55 +0000

    Linked list Palindrome

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -4,6 +4,31 @@
 #include "lists.h"
 
 /**
+* checkPalindrome - Function check a palindrome
+*
+* @left: pointer to left list
+*
+* @right: pointer to right list
+*
+* Return: res
+*
+*/
+
+
+int checkPalindrome(listint_t **left, listint_t  *right)
+{
+	int res;
+
+	if (right == NULL)
+		return (1);
+
+	res = checkPalindrome(left, right->next) && ((*left)->n == right->n);
+	(*left) = (*left)->next;
+
+	return (res);
+}
+
+/**
  * is_palindrome - Function checks if a singly linked list is a palindrome
  *
  * @head: Pointer to the head node(Initial).
@@ -13,5 +38,5 @@
 
 int is_palindrome(listint_t **head)
 {
-
+	return (checkPalindrome(head, *head));
 }
